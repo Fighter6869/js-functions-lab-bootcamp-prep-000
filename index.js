a1225c7b404e2bcdfd2c15c637c6b16f7239d783
getta <mulugetarobertson5@gmail.com>
2018-01-09 19:10:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays(string){
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(name)
+function happyHolidaysTo(string)
 {
-  return ("Happy holidays,"+'${name}!'); 
+  return ("Happy holidays,"+ ${name}!); 
 }
